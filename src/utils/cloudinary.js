523970e5b9f478acf3a9e90fabc978199a0ed78d
papai709 <shutterbug00111@gmail.com>
2026-04-24 08:44:19 +0530

    Add user register controller, user route and fix bug

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,30 +1,30 @@
-import {v2 as cloudinary} from "cloudinary"
-import fs from "fs"
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
 
-
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCloudinary = async (localFilePath) => {
+
+const uploadOnCloudinary = async (localFilePath)=>{
     try {
         if (!localFilePath) return null
         //upload the file on cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        // file has been uploaded successfull
-        console.log("file is uploaded on cloudinary ", response.url);
+        //file has been uploaded successfull
+        //console.log("File is uploaded on cloudinary ", response.url);
+        
+        fs.unlinkSync(localFilePath) // Remove the locally saved temporary file, as the upload operation was successful
         return response;
-
+        
     } catch (error) {
-        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        fs.unlinkSync(localFilePath) // Remove the locally saved temorary file, as the upload operation failed
         return null;
     }
 }
-
-
 
 export {uploadOnCloudinary}
